5b4f9ff166bb9cb4025d0ffcacce5fb52ff3f184
Satoshi Nagayasu <snaga@uptime.jp>
2015-01-20 03:30:54 +0000

    Add 9.0/9.1/9.2/9.3/9.4 support, drop 8.3/8.4 support.
    Error check improvement, and add exit code support.
    
    Test suite improvement.

--- test_mon_pgsql.sh
+++ test_mon_pgsql.sh
@@ -1,14 +1,79 @@
 #!/bin/sh
 
-MON_PGSQL_OPTS="-h localhost -p 5432 -U postgres -d postgres"
+_initdb()
+{
+    initdb -D /tmp/pgsql/data --no-locale -E UTF-8
+}
 
-./mon_pgsql $MON_PGSQL_OPTS session
-./mon_pgsql $MON_PGSQL_OPTS cache_hit
-./mon_pgsql $MON_PGSQL_OPTS tuple_wrtn
-./mon_pgsql $MON_PGSQL_OPTS tuple_read
-./mon_pgsql $MON_PGSQL_OPTS blks_read
-./mon_pgsql $MON_PGSQL_OPTS blks_wrtn
-./mon_pgsql $MON_PGSQL_OPTS txn
-./mon_pgsql $MON_PGSQL_OPTS xlog_wrtn
-./mon_pgsql $MON_PGSQL_OPTS dbsize
-./mon_pgsql $MON_PGSQL_OPTS locks
+_pgctl_start()
+{
+    pg_ctl -w -D /tmp/pgsql/data start
+}
+
+_pgctl_stop()
+{
+    pg_ctl -w -D /tmp/pgsql/data stop
+}
+
+_cleanup()
+{
+    rm -rf /tmp/pgsql/data
+}
+
+_runtest()
+{
+    monlog=$1
+    outlog=$2
+
+    cat /dev/null > output/$monlog
+    cat /dev/null > output/$outlog
+
+    MON_PGSQL_OPTS="$MON_PGSQL_OPTS -l output/$monlog"
+    export MON_PGSQL_OPTS
+
+    ./mon_pgsql $MON_PGSQL_OPTS session | tee -a output/$outlog
+    ./mon_pgsql $MON_PGSQL_OPTS cache_hit | tee -a output/$outlog
+    ./mon_pgsql $MON_PGSQL_OPTS tuple_wrtn | tee -a output/$outlog
+    ./mon_pgsql $MON_PGSQL_OPTS tuple_read | tee -a output/$outlog
+    ./mon_pgsql $MON_PGSQL_OPTS blks_read | tee -a output/$outlog
+    ./mon_pgsql $MON_PGSQL_OPTS blks_wrtn | tee -a output/$outlog
+    ./mon_pgsql $MON_PGSQL_OPTS txn | tee -a output/$outlog
+    ./mon_pgsql $MON_PGSQL_OPTS xlog_wrtn | tee -a output/$outlog
+    ./mon_pgsql $MON_PGSQL_OPTS dbsize | tee -a output/$outlog
+    ./mon_pgsql $MON_PGSQL_OPTS locks | tee -a output/$outlog
+}
+
+_testsuite()
+{
+    PGHOME=$1
+    export PGHOME
+    PATH=${PGHOME}/bin:$PATH
+    export PATH
+
+    echo PGHOME=$PGHOME
+
+    rm -rf /var/tmp/mon_pgsql.cache
+    _initdb
+    _pgctl_start
+    
+    _runtest $2 $3
+
+    diff -rc expected/$3 output/$3 | tee -a test_mon_pgsql.diff
+    
+    _pgctl_stop
+    _cleanup
+}
+
+#MON_PGSQL_OPTS="-h localhost -p 5432 -U postgres -d postgres"
+#MON_PGSQL_OPTS="-l mon_pgsql.log -v -h localhost -p 5432 -U $USER -d postgres"
+MON_PGSQL_OPTS="-v -U $USER -d postgres"
+export MON_PGSQL_OPTS
+
+rm test_mon_pgsql.diff
+rm *.log
+
+_testsuite /usr/pgsql-9.0 mon_pgsql90.log pgsql90.log
+_testsuite /usr/pgsql-9.1 mon_pgsql91.log pgsql91.log
+_testsuite /usr/pgsql-9.2 mon_pgsql92.log pgsql92.log
+_testsuite /usr/pgsql-9.3 mon_pgsql93.log pgsql93.log
+_testsuite /usr/pgsql-9.4 mon_pgsql94.log pgsql94.log
